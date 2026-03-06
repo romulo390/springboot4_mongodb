5f892a74d3994a2a7f3164acdc171b62235f2b29
romulo390 <cromuloaraujo@gmail.com>
2026-03-06 18:08:17 -0300

    primeiro commit

---
+++ src/main/java/com/romulo/databasemongo/DatabasemongoApplication.java
@@ -0,0 +1,13 @@
+package com.romulo.databasemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasemongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romulo/databasemongo/DatabasemongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romulo.databasemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasemongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
