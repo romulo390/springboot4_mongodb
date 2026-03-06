d92de0712c898c6e267f5bb9e072586b38e368bf
romulo390 <cromuloaraujo@gmail.com>
2026-03-06 18:49:27 -0300

    Criando UserController e UserDomain

--- src/main/java/com/romulo/databasemongo/controller/UserController.java
+++ src/main/java/com/romulo/databasemongo/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.romulo.databasemongo.controller;
 
+import com.romulo.databasemongo.domain.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController()
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping()
+    public ResponseEntity<List<User>> findAll() {
+            User romulo = new User("1","Romulo","romulo@tipo.com");
+            User ilza = new User("2","ilza","ilza@tipo.com");
+            List<User> userList = new ArrayList<>();
+            userList.addAll(Arrays.asList(romulo,ilza));
+            return ResponseEntity.ok().body(userList);
+            //OU - return  new ResponseEntity<>(userList, HttpStatus.OK);
+
+    }
 }
---
+++ src/main/java/com/romulo/databasemongo/domain/User.java
@@ -0,0 +1,57 @@
+package com.romulo.databasemongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class User implements Serializable {
+
+    private String id;
+    private String username;
+    private String email;
+
+    public User() {}
+
+    public User(String id, String username, String email) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
